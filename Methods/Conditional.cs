1665ed7cca0289e9bd1499ad308914d2427971b2
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-03-11 02:53:04 +0200

    Initial

---
+++ Methods/Conditional.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public static class Conditional
+    {
+        public static double ComparingAandBIfAisBiggerAddThemIfAequalsBMultiplyElseAMinusB(double a, double b)
+        {
+            double res;
+
+            if (a > b)
+            {
+                res = a + b;
+            }
+            else if (a == b)
+            {
+                res = a * b;
+            }
+            else
+            {
+                res = a - b;
+            }
+
+            return res;
+        }
+
+        public static string FindingPlaceOnACoordinatePlane(double x, double y)
+        {
+            string res = string.Empty;
+
+            if (x * y > 0)
+            {
+                if (x > 0)
+                {
+                    res = "1st quadrant";
+                }
+                else
+                {
+                    res = "3rd quadrant";
+                }
+            }
+            if (x * y < 0)
+            {
+                if (x > 0)
+                {
+                    res = "4th quadrant";
+                }
+                else
+                {
+                    res = "2nd quadrant";
+                }
+            }
+            if (x * y == 0)
+            {
+                if (x == 0 && y == 0)
+                {
+                    res = "The coordinate plane origin";
+                }
+                else if (x != 0)
+                {
+                    res = "X axis";
+                }
+                else
+                {
+                    res = "Y axis";
+                }
+            }
+
+            return res;
+        }
+
+        public static double[] AscendingNumberSorting(double a, double b, double c)
+        {
+            double[] res = null;
+
+            if (a > b && a > c)
+            {
+                if (b > c)
+                {
+                    res = new double[] { c, b, a };
+                }
+                else
+                {
+                    res = new double[] { b, c, a };
+                }
+            }
+            if (b > a && b > c)
+            {
+                if (a > c)
+                {
+                    res = new double[] { c, a, b };
+                }
+                else
+                {
+                    res = new double[] { a, c, b };
+                }
+            }
+            if (c > a && c > b)
+            {
+                if (a > b)
+                {
+                    res = new double[] { b, a, c }; ;
+                }
+                else
+                {
+                    res = new double[] { a, b, c };
+                }
+            }
+
+            return res;
+        }
+
+        public static double[] SolvingQuadraticEquation(double a, double b, double c)
+        {
+            double[] res = null;
+            double x;
+            double x2;
+            double d = b * b - (4 * a * c);
+
+            if (d >= 0)
+            {
+                if (d > 0)
+                {
+                    x = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+                    res = new double[] { x, x2 };
+                }
+                else
+                {
+                    x = -b / (2 * a);
+
+                    res = new double[] { x };
+                }
+            }
+            else
+            {
+
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Methods/Variables.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public static class Variables
+    {
+        public static double SolveEquation(double a, double b)
+        {
+            if (a == b)
+            {
+                throw new Exception("b - a = 0, can't divide by zero");
+            }
+
+            return (5 * a + b * b) / (b - a);
+        }
+
+        public static void SwapVariables(ref double a, ref double b)
+        {
+            double swapVariable;
+
+            swapVariable = a;
+            a = b;
+            b = swapVariable;
+        }
+
+        public static double[] DivideAndFindRemainder(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("can't divide by zero");
+            }
+
+            double[] divisionResultAndRemainder = new double[2] { a / b, a % b };
+
+            return divisionResultAndRemainder;
+        }
+
+        public static double SolveLinearEquation(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                throw new Exception("can't solve the equation if a = 0");
+            }
+
+            double x = (c - b) / a;
+
+            return x;
+        }
+
+        public static string MakeEquationOfALine(double x1, double y1, double x2, double y2)
+        {
+            if (x2 - x1 == 0)
+            {
+                throw new Exception("x2 - x1 = 0, can't divide by zero");
+            }
+
+            string equationOfALine = $"y = {(y2 - y1) / (x2 - x1)}x + ({-(x1 * y2 - x1 * y1) / (x2 - x1) + y1})";
+
+            return equationOfALine;
+        }
+
+    }
+}
