5e469d29763e454495d7749c1e7764b16cafc6ec
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-03-17 01:41:56 +0200

    Added tests for Arrays, TwoDimensionalArrays. Done with positive tests

---
+++ Methods.Tests/ArraysTests.cs
@@ -0,0 +1,120 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods.Tests
+{
+    class ArraysTests
+    {
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, -857)]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, -836)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, 0)]
+        [TestCase(new int[] { -1, 0, 1 }, -1)]
+        public void FindMinimumValueInArray_WhenArrayPassed_ReturnMinimumValueOfTheArray(int[] array, int expected)
+        {
+            int actual = Arrays.FindMinimumValueInArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, 137)]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, 17482)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, 4)]
+        [TestCase(new int[] { -1, 0, 1 }, 1)]
+        public void FindMaximumValueInArray_WhenArrayPassed_ReturnMaximumValueOfTheArray(int[] array, int expected)
+        {
+            int actual = Arrays.FindMaximumValueInArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, 7)]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, 2)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, 0)]
+        [TestCase(new int[] { -1, 0, 1 }, 0)]
+        public void FindMinimumValueIndexInArray_WhenArrayPassed_ReturnIndexOfTheMinimumValueOfTheArray(int[] array, int expected)
+        {
+            int actual = Arrays.FindMinimumValueIndexInArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, 6)]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, 5)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, 7)]
+        [TestCase(new int[] { -1, 0, 1 }, 2)]
+        public void FindMaximumValueIndexInArray_WhenArrayPassed_ReturnIndexOfTheMaximumValueOfTheArray(int[] array, int expected)
+        {
+            int actual = Arrays.FindMaximumValueIndexInArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 4, 6, 8, 34, 1, 8, 9, 0 }, 48)]
+        [TestCase(new int[] { 8, 4, 56, 7, 3, 5, 9, -2, -45, -2, -6, 6 }, 18)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 1 }, 0)]
+        public void ReturnSumOfNumbersWithOddIndexes_WhenArrayPassed_ReturnSumOfNumbersWithOddIndexes(int[] array, int expected)
+        {
+            int actual = Arrays.ReturnSumOfNumbersWithOddIndexes(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [TestCase(new int[] { -1, -2, -3 }, new int[] { -3, -2, -1 })]
+        [TestCase(new int[] { 0, 0, 0, -1 }, new int[] { -1, 0, 0, 0 })]
+        public void ReverseArray_WhenArrayPassed_ReturnReversedArray(int[] array, int[] expected)
+        {
+            int[] actual = Arrays.ReverseArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, 7)]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, 3)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, 0)]
+        [TestCase(new int[] { -1, 0, 1 }, 2)]
+        public void CountOddElementsOfTheArray_WhenArrayPassed_ReturnTheNumberOfOddElementsInTheArray(int[] array, int expected)
+        {
+            int actual = Arrays.CountOddElementsOfTheArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, new int[] { 5, 137, -857, 1, 3, 3, 7, 0, -4 })]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, new int[] { -57, -173, 17482, -134, -57, -836 })]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, new int[] { 0, 0, 0, 4, 0, 0, 0, 0 })]
+        [TestCase(new int[] { -1, 0, 1 }, new int[] { 1, 0, -1 })]
+        public void ChangePlacesOfArrayHalves_WhenArrayPassed_ReturnArrayWithChangedPlacesOfArrayHalves(int[] array, int[] expected)
+        {
+            int[] actual = Arrays.ChangePlacesOfArrayHalves(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, new int[] { -857, -4, 0, 1, 3, 3, 5, 7, 137 })]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, new int[] { -836, -173, -134, -57, -57, 17482 })]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 4 })]
+        [TestCase(new int[] { -1, 0, 1 }, new int[] { -1, 0, 1 })]
+        public void SortArrayAscending_WhenArrayPassed_ReturnArraySortedInAscendingOrder(int[] array, int[] expected)
+        {
+            int[] actual = Arrays.SortArrayAscending(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { 3, 7, 0, -4, 3, 5, 137, -857, 1 }, new int[] { 137, 7, 5, 3, 3, 1, 0, -4, -857 })]
+        [TestCase(new int[] { -134, -57, -836, -57, -173, 17482 }, new int[] { 17482, -57, -57, -134, -173, -836 })]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 4 }, new int[] { 4, 0, 0, 0, 0, 0, 0, 0 })]
+        [TestCase(new int[] { -1, 0, 1 }, new int[] { 1, 0, -1 })]
+        public void SortArrayDescending_WhenArrayPassed_ReturnArraySortedInDescendingOrder(int[] array, int[] expected)
+        {
+            int[] actual = Arrays.SortArrayDescending(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Methods.Tests/TwoDimensionalArraysTests.cs
@@ -0,0 +1,164 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods.Tests
+{
+    class TwoDimensionalArraysTests
+    {
+        [TestCase(1, 10)]
+        [TestCase(2, -9)]
+        [TestCase(3, 0)]
+        public void FindMinimumValueInATwoDimensionalArray_WhenArrayPassed_ReturnMinimumValueOfTheArray(int mockNumber, int expected)
+        {
+            int[,] array = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(mockNumber);
+
+            int actual = TwoDimensionalArrays.FindMinimumValueInATwoDimensionalArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 99)]
+        [TestCase(2, 9)]
+        [TestCase(3, 0)]
+        public void FindMaximumValueInATwoDimensionalArray_WhenArrayPassed_ReturnMaximumValueOfTheArray(int mockNumber, int expected)
+        {
+            int[,] array = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(mockNumber);
+
+            int actual = TwoDimensionalArrays.FindMaximumValueInATwoDimensionalArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, new int[] { 0, 0 })]
+        [TestCase(2, new int[] { 2, 2 })]
+        [TestCase(3, new int[] { 0, 0 })]
+        public void FindMinimumValueIndexInATwoDimensionalArray_WhenArrayPassed_ReturnIndexOfTheMinimumValueOfTheArray(int mockNumber, int[] expected)
+        {
+            int[,] array = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(mockNumber);
+
+            int[] actual = TwoDimensionalArrays.FindMinimumValueIndexInATwoDimensionalArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, new int[] { 3, 2 })]
+        [TestCase(2, new int[] { 1, 0 })]
+        [TestCase(3, new int[] { 0, 0 })]
+        public void FindMaximumValueIndexInATwoDimensionalArray_WhenArrayPassed_ReturnIndexOfTheMaximumValueOfTheArray(int mockNumber, int[] expected)
+        {
+            int[,] array = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(mockNumber);
+
+            int[] actual = TwoDimensionalArrays.FindMaximumValueIndexInATwoDimensionalArray(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 4)]
+        [TestCase(2, 3)]
+        [TestCase(3, 0)]
+        public void CountNumbersInAMatrixThatAreBiggerThanAllTheirNeighbours_WhenArrayPassed_ReturnNumberOfArrayElementsThatAreBiggerThanAllTheirNeighbours(int mockNumber, int expected)
+        {
+            int[,] array = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(mockNumber);
+
+            int actual = TwoDimensionalArrays.CountNumbersInAMatrixThatAreBiggerThanAllTheirNeighbours(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(4, 5)]
+        [TestCase(2, 6)]
+        [TestCase(3, 7)]
+        public void TransposeAMatrix_WhenArrayPassed_ReturnATransposedMatrix(int mockNumber, int expectedMockNumber)
+        {
+            int[,] array = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(mockNumber);
+
+            int[,] actual = TwoDimensionalArrays.TransposeAMatrix(array);
+
+            int[,] expected = TwoDimensionalArrayMock.GetTwoDimensionalArrayMock(expectedMockNumber);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static class TwoDimensionalArrayMock
+        {
+            public static int[,] GetTwoDimensionalArrayMock(int number)
+            {
+                int[,] result = new int[0, 0];
+
+                switch (number)
+                {
+                    case 1:
+                        result = new int[,]
+                        {
+                            { 10, 60, 80, 90, 10, 11, 76 },
+                            { 40, 32, 93, 80, 11, 41, 51 },
+                            { 31, 20, 63, 44, 95, 71, 40 },
+                            { 64, 48, 99, 13, 11, 50, 16 },
+                            { 13, 65, 78, 45, 14, 15, 16 }
+                        };
+                        break;
+                    case 2:
+                        result = new int[,]
+                        {
+                            { 1, 5, 8, 4 },
+                            { 9, 7, 4, 1 },
+                            { -8, 3, -9, 6 }
+                        };
+                        break;
+                    case 3:
+                        result = new int[,]
+                        {
+                            { 0, 0, 0 },
+                            { 0, 0, 0 },
+                            { 0, 0, 0 },
+                            { 0, 0, 0 },
+                        };
+                        break;
+                    case 4:
+                        result = new int[,]
+                        {
+                            { 2, 6, 8, 0, 4, 3, 2, 1 },
+                            { 2, 6, 8, 0, 4, 3, 2, 1 },
+                            { 2, 6, 8, 0, 4, 3, 2, 1 },
+                            { 2, 6, 8, 0, 4, 3, 2, 1 }
+                        };
+                        break;
+                    case 5:
+                        result = new int[,]
+                        {
+                            { 2,2,2,2 },
+                            { 6,6,6,6 },
+                            { 8,8,8,8 },
+                            { 0,0,0,0 },
+                            { 4,4,4,4 },
+                            { 3,3,3,3 },
+                            { 2,2,2,2 },
+                            { 1,1,1,1 },
+                        };
+                        break;
+                    case 6:
+                        result = new int[,]
+                        {
+                            { 1, 9, -8},
+                            { 5, 7, 3},
+                            { 8, 4, -9},
+                            { 4, 1, 6},
+                        };
+                        break;
+                    case 7:
+                        result = new int[,]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 }
+                        };
+                        break;
+                }
+
+                return result;
+            }
+        }
+    }
+}
